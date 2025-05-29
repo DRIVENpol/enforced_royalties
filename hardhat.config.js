ebeffe95118cfc387c199bf20632b21891c4353d
Paul <84129217+DRIVENpol@users.noreply.github.com>
2025-05-29 13:24:53 +0300

    BuildBear -> .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,13 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
-const deployerKey = process.env.DEPLOYER;
-const buyerKey = process.env.BUYER;
-const sellerKey = process.env.SELLER;
-
 module.exports = {
   solidity: "0.8.27",
   networks: {
     buildbear: {
-      url: "https://rpc.buildbear.io/tasty-carnage-74b19342",
-      chainId: 26409,
-      accounts: [deployerKey, buyerKey, sellerKey]
+      url: process.env.BUILDBEAR_RPC_URL,
+      chainId: parseInt(process.env.BUILDBEAR_CHAIN_ID),
+      accounts: [process.env.DEPLOYER, process.env.BUYER, process.env.SELLER].filter(Boolean)
     }
   }
 };
